7d59f6be1f4717d1d8e9fa6c3968331f080966e0
Seungwoo, Joo <thisis.joos@gmail.com>
2023-12-28 17:37:00 +0900

    231228/1736

--- base-algorithm/review/review-template.py
+++ base-algorithm/review/review-template.py
@@ -5,6 +5,8 @@
 4. merge sort
 5. binary search - recursive
 6. binary search - iteration
+7. Dijkstra shortest-path algorithm
+8. Floyd-Warshall shortest-path algorithm
 
 arr = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]            # for sort
 arr = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21]   # for search
--- main.py
+++ main.py
@@ -1,1 +1,37 @@
-print(list(map(int, input())))
+import heapq
+INF = int(1e9)
+
+n, m = map(int, input().split())
+start = int(input())
+graph = [[] for i in range(n+1)]
+distance = [INF] * (n+1)
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a].append((b, c))
+
+
+def dijkstra(start):
+    q = []
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+
+    while q:
+        dist, now = heapq.heappop(q)
+        if distance[now] < dist:
+            continue
+
+        for n, d in graph[now]:
+            cost = dist + d
+            if cost < distance[n]:
+                distance[n] = cost
+                heapq.heappush(q, (cost, n))
+
+dijkstra(start)
+
+for i in range(1, n+1):
+    # 도달할 수 없는 경우, 무한(INF)라고 출력
+    if distance[i] == INF:
+        print("INF")
+    else:
+        print(distance[i], end=' ')
