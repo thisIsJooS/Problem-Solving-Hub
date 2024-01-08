10cff1c7270c6c58b997e2d9751bd117e3739337
Seungwoo, Joo <thisis.joos@gmail.com>
2024-01-08 13:44:21 +0900

    240108/1344

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-arr = [1, 3, 4]
-print(arr.index(2))
+n = int(input())
+arr = [[0]*(100) for _ in range(100)]
+
+for _ in range(n):
+    x, y = map(int, input().split())
+
+    for i in range(x, x+10):
+        for j in range(y, y+10):
+            arr[i][j] = 1
+
+
+cnt = 0
+for a in arr:
+    cnt += a.count(1)
+
+print(cnt)
