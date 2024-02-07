61bc0a89a06fddb33d4342e5f1a86a1dacba7268
Seungwoo, Joo <thisis.joos@gmail.com>
2024-02-07 22:42:17 +0900

    240207/2242

---
+++ boj/1silver/11403.py
@@ -0,0 +1,25 @@
+n = int(input())
+
+graph = [list(map(int, input().split())) for _ in range(n)]
+
+for i in range(n):
+    for j in range(n):
+        if graph[i][j] == 0:
+            graph[i][j] = 1e6
+
+for i in range(n):
+    graph[i][i] = 0
+
+for k in range(n):
+    for i in range(n):
+        for j in range(n):
+            graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+
+for i in range(n):
+    for j in range(n):
+        if graph[i][j] != 1e6:
+            print(1, end=' ')
+        else:
+            print(0, end=' ')
+    print()
+
