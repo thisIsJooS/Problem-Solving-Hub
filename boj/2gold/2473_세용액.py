4c8b80c97a86cf65bcee5958e979adeb87a26573
Seungwoo, Joo <thisis.joos@gmail.com>
2024-10-03 23:05:44 +0900

    BOJ 2473 세용액

---
+++ boj/2gold/2473_세용액.py
@@ -0,0 +1,33 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+arr.sort()
+
+"""
+3개를 섞어서 합이 0에 제일 가까운거 
+"""
+
+res = None
+val = 1e9
+
+for i in range(n-2):
+    start = i+1
+    end = n-1
+
+    while start < end:
+        agg = arr[i] + arr[start] + arr[end]
+
+        if abs(agg) < val:
+            val = abs(agg)
+            res = (arr[i], arr[start], arr[end])
+
+        if agg < 0:
+            start += 1
+        elif agg > 0:
+            end -= 1
+        else:
+            break
+
+print(*res)
+
+
